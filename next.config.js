aba0254c6cadadcf668fa41aa78d5b6423e87699
karthik <karthik.nishanth06@gmail.com>
2023-09-26 11:27:57 +0100

    changes

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = { basePath: "/staging.profici.co.uk" };
 
-module.exports = nextConfig
+module.exports = nextConfig;
