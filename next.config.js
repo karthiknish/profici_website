3bf0c444000eb5a93efd7f17a2cc44ec7e936f47
karthik <karthik.nishanth06@gmail.com>
2023-09-27 12:19:29 +0100

    dewf

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: process.argv.includes("export") ? "/profici" : "",
+  assetPrefix: process.argv.includes("export") ? "./" : "",
 
   images: { unoptimized: true },
 };
