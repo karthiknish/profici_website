f1098110d0cc8ce6a9c968d8e76df4959f413e08
karthik <karthik.nishanth06@gmail.com>
2023-09-27 10:45:20 +0100

    image error

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { assetPrefix: "./" };
+const nextConfig = { assetPrefix: "./", images: { unoptimized: true } };
 
 module.exports = nextConfig;
