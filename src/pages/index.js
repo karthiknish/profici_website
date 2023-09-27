2ecce2db8369fe082a30f4ec4a9ca2b9036f9807
karthik <karthik.nishanth06@gmail.com>
2023-09-27 10:43:26 +0100

    home changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Head from "next/head";
 import { motion } from "framer-motion";
 // Add required imports for images or fetch data as needed
 
@@ -18,11 +19,70 @@ function HomePage() {
   );
 
   return (
-    <main>
-      {/* First Section */}
-      <section className="bg-primary text-white py-16 lg:py-24 relative overflow-hidden">
-        <div className="flex flex-wrap justify-center items-center px-12 lg:px-24 relative z-10">
-          <div className="w-full md:w-1/2 px-8 text-center md:text-left">
+    <>
+      <Head>
+        <title>Profici</title>
+      </Head>
+      <main>
+        {/* First Section */}
+        <section className="bg-primary text-white py-16 lg:py-24 relative overflow-hidden">
+          <div className="flex flex-wrap justify-center items-center px-12 lg:px-24 relative z-10">
+            <div className="w-full md:w-1/2 px-8 text-center md:text-left">
+              <p className="mb-4">
+                Elevate, Engage, Excel: Your Journey Begins Here.
+              </p>
+              <h1 className="text-4xl mb-4">
+                Your Digital Presence Is About To Take Off
+              </h1>
+              <p className="mb-6">
+                Propelling businesses into the digital age by transforming
+                clicks into loyal customers and driving unparalleled growth.
+              </p>
+              <a
+                href="/about-us"
+                className="bg-white text-primary px-6 py-2 rounded-full inline-flex items-center"
+              >
+                About Us
+                {/* SVG Icon */}
+              </a>
+            </div>
+            <div className="w-full md:w-1/2 mb-8 md:mb-0">
+              <img
+                src="https://staging.profici.co.uk/newprofici/wp-content/uploads/2023/08/Profici-Guys-Talking.png"
+                alt="Profici Guys Talking"
+                className="w-full max-w-sm mx-auto"
+              />
+            </div>
+          </div>
+          <svg
+            viewBox="0 0 100 100"
+            preserveAspectRatio="none"
+            style={{
+              position: "absolute",
+              bottom: 0,
+              left: 0,
+              width: "100%",
+              height: "30%",
+              zIndex: 1,
+            }}
+          >
+            <path
+              d="M0 80 C40 60 30 30 100 0 L100 100 L0 100 Z"
+              fill="white"
+            ></path>
+          </svg>
+        </section>
+
+        {/* Second Section */}
+        <section className="flex flex-wrap-reverse py-16 lg:py-24 px-12 lg:px-24">
+          <div className="w-full md:w-1/2 px-8">
+            <img
+              src="https://staging.profici.co.uk/newprofici/wp-content/uploads/2023/08/TomProficiEvent.png"
+              alt="Tom at Profici Event"
+              className="w-full max-w-sm mx-auto"
+            />
+          </div>
+          <div className="w-full md:w-1/2 px-8">
             <p className="mb-4">
               Elevate, Engage, Excel: Your Journey Begins Here.
             </p>
@@ -33,131 +93,77 @@ function HomePage() {
               Propelling businesses into the digital age by transforming clicks
               into loyal customers and driving unparalleled growth.
             </p>
-            <a
-              href="/about-us"
-              className="bg-white text-primary px-6 py-2 rounded-full inline-flex items-center"
-            >
-              About Us
-              {/* SVG Icon */}
-            </a>
           </div>
-          <div className="w-full md:w-1/2 mb-8 md:mb-0">
+        </section>
+
+        {/* Services Section */}
+        <section className="py-16 lg:py-24 px-12 lg:px-24 bg-gray-100">
+          <h2 className="text-center text-4xl mb-16">Services</h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12">
+            {services.map((service, index) => (
+              <div
+                key={index}
+                className="bg-white p-6 rounded-lg shadow-md hover:shadow-xl transform transition-transform duration-300 hover:-translate-y-2.5"
+              >
+                <img
+                  src={service.image}
+                  alt={service.title}
+                  className="mb-4 w-1/4 mx-auto"
+                />
+                <h3 className="font-bold text-xl mb-2">{service.title}</h3>
+                <p className="text-gray-600">{service.description}</p>
+              </div>
+            ))}
+          </div>
+        </section>
+
+        {/* Email Subscription Section */}
+        <section className="flex flex-wrap py-16 lg:py-24 px-12 lg:px-24">
+          <div className="w-full lg:w-1/2 px-8 lg:pr-16">
+            <p className="mb-4 text-lg">Your go-to marketing guide</p>
+            <h1 className="text-4xl mb-6">
+              Download your Free 26-Page Website Analysis Report
+            </h1>
+            <div className="flex flex-col md:flex-row">
+              <input
+                type="email"
+                placeholder="Enter your email"
+                className="p-2 flex-grow border rounded-l-md"
+              />
+              <button className="bg-primary text-white px-6 py-2 rounded-r-md">
+                Download
+              </button>
+            </div>
+          </div>
+          <div className="w-full lg:w-1/2 px-8 lg:pl-16">
             <img
-              src="https://staging.profici.co.uk/newprofici/wp-content/uploads/2023/08/Profici-Guys-Talking.png"
-              alt="Profici Guys Talking"
-              className="w-full max-w-sm mx-auto"
+              src="https://staging.profici.co.uk/newprofici/wp-content/uploads/2023/08/Marketing-Phone-Graphic.png"
+              alt="Marketing Phone Graphic"
+              className="w-full max-w-md mx-auto"
             />
           </div>
-        </div>
-        <svg
-          viewBox="0 0 100 100"
-          preserveAspectRatio="none"
-          style={{
-            position: "absolute",
-            bottom: 0,
-            left: 0,
-            width: "100%",
-            height: "30%",
-            zIndex: 1,
-          }}
-        >
-          <path
-            d="M0 80 C40 60 30 30 100 0 L100 100 L0 100 Z"
-            fill="white"
-          ></path>
-        </svg>
-      </section>
+        </section>
 
-      {/* Second Section */}
-      <section className="flex flex-wrap-reverse py-16 lg:py-24 px-12 lg:px-24">
-        <div className="w-full md:w-1/2 px-8">
-          <img
-            src="https://staging.profici.co.uk/newprofici/wp-content/uploads/2023/08/TomProficiEvent.png"
-            alt="Tom at Profici Event"
-            className="w-full max-w-sm mx-auto"
-          />
-        </div>
-        <div className="w-full md:w-1/2 px-8">
-          <p className="mb-4">
-            Elevate, Engage, Excel: Your Journey Begins Here.
-          </p>
-          <h1 className="text-4xl mb-4">
-            Your Digital Presence Is About To Take Off
-          </h1>
-          <p className="mb-6">
-            Propelling businesses into the digital age by transforming clicks
-            into loyal customers and driving unparalleled growth.
-          </p>
-        </div>
-      </section>
-
-      {/* Services Section */}
-      <section className="py-16 lg:py-24 px-12 lg:px-24 bg-gray-100">
-        <h2 className="text-center text-4xl mb-16">Services</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12">
-          {services.map((service, index) => (
-            <div
-              key={index}
-              className="bg-white p-6 rounded-lg shadow-md hover:shadow-xl transform transition-transform duration-300 hover:-translate-y-2.5"
-            >
-              <img
-                src={service.image}
-                alt={service.title}
-                className="mb-4 w-1/4 mx-auto"
-              />
-              <h3 className="font-bold text-xl mb-2">{service.title}</h3>
-              <p className="text-gray-600">{service.description}</p>
-            </div>
-          ))}
-        </div>
-      </section>
-
-      {/* Email Subscription Section */}
-      <section className="flex flex-wrap py-16 lg:py-24 px-12 lg:px-24">
-        <div className="w-full lg:w-1/2 px-8 lg:pr-16">
-          <p className="mb-4 text-lg">Your go-to marketing guide</p>
-          <h1 className="text-4xl mb-6">
-            Download your Free 26-Page Website Analysis Report
-          </h1>
-          <div className="flex flex-col md:flex-row">
-            <input
-              type="email"
-              placeholder="Enter your email"
-              className="p-2 flex-grow border rounded-l-md"
-            />
-            <button className="bg-primary text-white px-6 py-2 rounded-r-md">
-              Download
-            </button>
+        {/* Clients Section */}
+        <section className="py-16 bg-gray-100 lg:py-24 px-16 lg:px-32">
+          <h2 className="text-center text-4xl mb-16">Our Clients</h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+            {clients.map((client, index) => (
+              <div
+                key={index}
+                className="transform transition-transform duration-300 p-2"
+              >
+                <img
+                  src={client}
+                  alt="Client Logo"
+                  className="w-1/3 mx-auto filter grayscale hover:filter-none"
+                />
+              </div>
+            ))}
           </div>
-        </div>
-        <div className="w-full lg:w-1/2 px-8 lg:pl-16">
-          <img
-            src="https://staging.profici.co.uk/newprofici/wp-content/uploads/2023/08/Marketing-Phone-Graphic.png"
-            alt="Marketing Phone Graphic"
-            className="w-full max-w-md mx-auto"
-          />
-        </div>
-      </section>
-
-      {/* Clients Section */}
-      <section className="py-16 bg-gray-100 lg:py-24 px-16 lg:px-32">
-        <h2 className="text-center text-4xl mb-16">Our Clients</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-          {clients.map((client, index) => (
-            <div
-              key={index}
-              className="transform transition-transform duration-300 p-2"
-            >
-              <img
-                src={client}
-                alt="Client Logo"
-                className="w-1/3 mx-auto filter grayscale hover:filter-none"
-              />
-            </div>
-          ))}
-        </div>
-      </section>
-    </main>
+        </section>
+      </main>
+    </>
   );
 }
 export default HomePage;
---
+++ src/pages/who-we-are/index.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { motion } from "framer-motion";
+import Head from "next/head";
+import Link from "next/link";
+
+export default function WhoWeAre({ teamMembers }) {
+  return (
+    <>
+      <Head>
+        <title>Who We Are</title>
+      </Head>
+      <main className="py-16 px-4 sm:px-8 lg:px-24">
+        <h1 className="text-4xl mb-2 text-center">Who We Are</h1>
+        <p className="text-center mb-12 text-gray-600">
+          Part family, part well-oiled machine; our team is a close-knit group
+          of talented individuals who collaborate to craft bold designs through
+          a proven process – including equal parts strategy and design. Our goal
+          is to support, enhance and amplify your brand by designing meaningful
+          user experiences that are authentic to your brand’s voice and culture.
+        </p>
+
+        <div className="grid mt-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 sm:gap-12">
+          {teamMembers.map((member) => (
+            <Link
+              className="block bg-white shadow-lg rounded-lg overflow-hidden hover:opacity-80 transition-opacity duration-200"
+              key={member.name}
+              href={`/who-we-are/${encodeURIComponent(
+                member.name
+              ).toLowerCase()}`}
+            >
+              <img
+                src={member.image}
+                alt={member.name}
+                className="w-full object-cover"
+              />
+              <div className="p-4 sm:p-6">
+                <h2 className="text-2xl mb-2">{member.name}</h2>
+                <p className="text-lg text-gray-600">{member.position}</p>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </main>
+    </>
+  );
+}
+
+export async function getStaticProps() {
+  const teamMembers = await import("../../../public/team.json");
+  return {
+    props: {
+      teamMembers: teamMembers.default,
+    },
+  };
+}
