653796f9c3061cf43d226419491170f73c5c182c
karthik <karthik.nishanth06@gmail.com>
2023-09-27 12:12:59 +0100

    change export

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: process.argv.includes("export") ? "/profici" : "",
   basePath: process.argv.includes("export") ? "/profici" : "",
   images: { unoptimized: true },
 };
